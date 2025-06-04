714311a99f89e3e1f3b6aef9659c8f4461495b64
zulfiqar <visiosoft@yahoo.com>
2025-06-04 04:16:41 -0700

    ad

--- app/src/main/java/com/mpo/magicquiz/MainActivity.kt
+++ app/src/main/java/com/mpo/magicquiz/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mpo.magicquiz
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -15,6 +16,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
